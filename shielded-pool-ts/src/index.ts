fa796bbdcd27ad75f3b05fac49876023f90284f0
Todd Norton <tnorton314@protonmail.com>
2025-06-22 23:38:25 +0200

    hack: ignore type-widening bug

--- shielded-pool-ts/src/index.ts
+++ shielded-pool-ts/src/index.ts
@@ -2,5 +2,8 @@ import { UltraHonkBackend } from "@aztec/bb.js";
 import { Noir } from "@noir-lang/noir_js";
 import circuit from "../../circuit/target/shielded_pool.json";
 
+// Type-widening bug causes tsc to misinterpret
+// AbiType { "kind": "field" } as { kind: string }
+// @ts-ignore
 const noir = new Noir(circuit);
 const backend = new UltraHonkBackend(circuit.bytecode);
