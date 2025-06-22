b8b5548112460cc1239b662d8cead7203574e238
Todd Norton <tnorton314@protonmail.com>
2025-06-23 01:46:02 +0200

    chore[ts]: convert Asset to class

--- shielded-pool-ts/src/asset.ts
+++ shielded-pool-ts/src/asset.ts
@@ -1,4 +1,25 @@
-export type Asset = {
-  id: number; // todo: hex string?
-  amount: number;
-};
+export class Asset {
+  public id: bigint;
+  public amount: bigint;
+
+  constructor(id: bigint | number, amount: bigint | number) {
+    if (typeof id === "number") {
+      this.id = BigInt(id);
+    } else {
+      this.id = id;
+    }
+    if (typeof amount === "number") {
+      this.amount = BigInt(amount);
+    } else {
+      this.amount = amount;
+    }
+    // TODO: same range check that circuit enforces on amount
+  }
+
+  toCircuitInput() {
+    return {
+      id: this.id.toString(),
+      amount: this.amount.toString(),
+    };
+  }
+}
