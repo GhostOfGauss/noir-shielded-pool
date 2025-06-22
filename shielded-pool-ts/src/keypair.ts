c23d1c1efe42cc79cd61d164b50a1faa430f1914
Todd Norton <tnorton314@protonmail.com>
2025-06-23 01:17:10 +0200

    feat[ts]: keypair

---
+++ shielded-pool-ts/src/keypair.ts
@@ -0,0 +1,37 @@
+// Embedded curve scalar field in its
+// non-native representation as limbs
+// valued in the proof system scalar field.
+export class SecretKey {
+  constructor(
+    public lo: bigint,
+    public hi: bigint
+  ) {}
+
+  toCircuitInput() {
+    return { lo: this.lo.toString(), hi: this.hi.toString() };
+  }
+}
+
+// Embedded curve point represented
+// in affine coordinates over the
+// proof system scalar field.
+export class PublicKey {
+  constructor(
+    public x: bigint,
+    public y: bigint,
+    public is_infinite: boolean
+  ) {
+    // TODO: check well-constructed
+  }
+
+  /**
+   * Derive public key from secret key
+   * by multiplying generator point.
+   */
+  static derive(sk: SecretKey): PublicKey {
+    // TODO: this is backend dependent, sometimes
+    // BB uses Grumpkin, sometimes BabyJubJub as
+    // embedded curve.
+    throw "unimpl";
+  }
+}
